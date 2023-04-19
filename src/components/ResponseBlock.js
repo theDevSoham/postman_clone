723ecb1a8a673b2e93be99900f19b6384263c993
Soham Das <publicsoham.24@gmail.com>
2023-04-19 19:36:22 +0530

    pretty bytes installed

--- src/components/ResponseBlock.js
+++ src/components/ResponseBlock.js
@@ -9,12 +9,15 @@ import {
   Text,
 } from "@chakra-ui/react";
 import React from "react";
+import PrettyBytes from 'react-pretty-bytes';
 import RespHeaders from "./RespHeaders";
 import RespBody from "./RespBody";
 
 const ResponseBlock = ({ data }) => {
   const [response, setResponse] = React.useState({});
   const [headers, setHeaders] = React.useState({});
+  //const [body, setBody] = React.useState({});
+  const [size, setSize] = React.useState(0);
 
   React.useEffect(() => {
     if (typeof data === "object") {
@@ -30,6 +33,10 @@ const ResponseBlock = ({ data }) => {
 
     if (typeof response.headers !== "undefined" && typeof response.headers === "object") {
       setHeaders(response.headers);
+    }
+
+    if(typeof response.headers !== "undefined" && typeof response.data !== "undefined") {
+      setSize(JSON.stringify(response.data).length + JSON.stringify(response.headers).length);
     }
   }, [response]);
 
@@ -51,10 +58,20 @@ const ResponseBlock = ({ data }) => {
         display="flex"
         justifyContent="space-evenly"
         alignItems="center"
+        px={0}
       >
-        <Text>Status: {typeof response.status !== "undefined" ? response.status : "none"}</Text>
-        <Text>Time: {typeof response.customData?.time === "undefined" ? 0 : response.customData?.time} ms</Text>
-        <Text>Size: 100B</Text>
+        <Container display="flex" justifyContent="center" alignItems="center">
+          <Text>Status: {typeof response.status !== "undefined" ? response.status : "none"}</Text>
+        </Container>
+        <Container display="flex" justifyContent="center" alignItems="center">
+          <Text>Time: {typeof response.customData?.time === "undefined" ? 0 : response.customData?.time} ms</Text>
+        </Container>
+        <Container display="flex" justifyContent="center" alignItems="center">
+          <Text>Size: </Text>
+          <Container>
+            <PrettyBytes bytes={size} />
+          </Container>
+        </Container>
       </Container>
       <Container height="60%" px={0}>
         <Tabs size="md" variant="enclosed">
